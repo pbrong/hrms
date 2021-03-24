a81884f8ecf12330d424963e208a899101b79bde
pbrong <1378789620@qq.com>
2021-03-24 23:02:05 +0800

    feature:完成普通员工信息查询及通知查询

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ go build -o hrms_app/macos_app main.go
 chmod a+x hrms_app/macos_app
 
 echo "[start to build windows_app]"
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o hrms_app/windows_app main.go
-chmod a+x hrms_app/windows_app
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o hrms_app/windows_app.exe main.go
+chmod a+x hrms_app/windows_app.exe
 
 echo "[start to build linux_app]"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o hrms_app/linux_app main.go
